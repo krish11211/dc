6b66a3481ad24b2fabf4dcca9be2b8e2ee52d332
Krish gurung <107617991+krish11211@users.noreply.github.com>
2023-11-20 10:26:20 +0000

    edited

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
 import './about.css'
 const About = () =>{
     return(
-        <div className="aboutp"><h1>About page</h1></div>
+        <div className="aboutp"><h1>About pages</h1></div>
     )
 }
 export default About
